cac60fae2e1f5c4346ad666d1aaea91bb086ea09
NightDriver999 <andrey.ionov2013@yandex.ru>
2023-03-02 16:50:11 +0300

    First commit

---
+++ src/components/v-cart-item.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="v-cart-item">
+    <img
+      class="v-cart-item__image"
+      :src="require('../assets/images/' + cart_item_data.image)"
+      alt="picture"
+    />
+    <div>
+      <p>{{ cart_item_data.name }}</p>
+      <p>{{ cart_item_data.price }}</p>
+      <p>{{ cart_item_data.article }}</p>
+      <p>{{ cart_item_data.about }}</p>
+    </div>
+    <div class="v-cart-item__quantity">
+      <p>Quantity</p>
+      <span>
+        {{ cart_item_data.quantity }}
+      </span>
+    </div>
+    <button class="btn" @click="deleteFromCart">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-cart-item",
+  props: {
+    cart_item_data: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      title: "hello!",
+    };
+  },
+  computed: {},
+  methods: {
+    deleteFromCart() {
+        this.$emit("deleteFromCart");
+    },
+  },
+  watch: {},
+  mounted() {
+  },
+};
+</script>
+
+<style lang="scss">
+.v-cart-item {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 grey;
+  padding: 20px;
+  margin: 20px;
+
+  &__image {
+    min-width: 10%;
+  }
+}
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="v-main-wrapper">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from "vuex";
+
+export default {
+    name: "v-main-wrapper",
+    props: {},
+    data() {
+        return {
+            title: "hello!",
+        };
+    },
+    computed: {
+        ...mapGetters(["CART"]),
+    },
+    methods: {},
+    watch: {},
+    mounted() {
+    },
+}; 
+</script>
+
+<style>
+
+.v-main-wrapper {
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+</style>
